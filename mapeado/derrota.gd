f231944c43d72172be4c4383c17889f71b288ace
LuisScuffi <Luisscuffi@gmail.com>
2024-11-08 16:46:46 -0300

    1234

--- mapeado/derrota.gd
+++ mapeado/derrota.gd
@@ -1,9 +1,7 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Ui.vidas_caballero = 3
 	await get_tree().create_timer(6).timeout
 	Transicion.Transicion("res://Mapas/Mapa 1.tscn")
-	pass # Replace with function body.
+	pass
